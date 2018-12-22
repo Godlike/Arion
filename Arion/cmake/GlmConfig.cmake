008098cde0e763d4b85604af90f253dfeb9e091a
Vitali Parkhomenko <parkhomenko.mail@gmail.com>
2018-12-22 17:39:13 +0300

    [#38] Update submodules

--- Arion/cmake/GlmConfig.cmake
+++ Arion/cmake/GlmConfig.cmake
@@ -4,6 +4,7 @@
 
 set(GLM_INSTALL_ENABLE OFF CACHE BOOL "Flag to override default GLM_INSTALL_ENABLE value")
 set(GLM_TEST_ENABLE OFF CACHE BOOL "Flag to override default GLM_TEST_ENABLE value")
+add_compile_definitions(GLM_FORCE_SILENT_WARNINGS)
 
 if (NOT DEFINED GLM_ROOT_DIR)
     set(GLM_ROOT_DIR
